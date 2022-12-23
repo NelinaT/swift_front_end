8ef238f5b2f3309c361992c39342da973c73e52a
Nelina Toteva <nelinatoteva@gmail.com>
2022-12-23 02:11:27 +0200

    score fixed

--- cardsGame/js/game.js
+++ cardsGame/js/game.js
@@ -1,33 +1,22 @@
-
-// var table = document.createElement("div");
-// table.classList.add("table");
-// document.body.appendChild(table);
 var path="../images/Card_Hover_" ;
 var imageArray=["22.png","23.png","24.png","25.png","26.png","28.png", "29.png","30.png", "31.png"];
 var usedCards;
 var cells;
 var newCard;
 var addPoints;
-var score;
-var highScore=0;
+var score =$("span#score");
+var highScore=$("span#highScore");;
 var resulWrapper=$(".wrapper");
 var allTimeBest=$("#allTimeBest");
-score=$("span#score");
-highScore=$("span#highScore");
-// highScore.text(localStorage.highScore());
+var closedCard=path +"10.png";
 
 htmlGenerator();
-
-// $("img").click(function(){
-//     debugger
-//     $(this).toggleClass("active");
-// })
 
 $(".table").one("click",function(){
     $(".timer div").animate( {width: "0"},
     60000,
     function(){
-        //redirect here
+        
        window.location.href="gameOver.html?points="+score.text();
     
     })
@@ -44,7 +33,6 @@ function htmlGenerator(){
             class: "col "}).clone().appendTo(".row").on("click", openCard);   
     }
     cardCell();
-    // var localHighScore= localStorage.highScore
     if(localStorage.highScore){
         highScore.text(localStorage.highScore);
     }
@@ -63,7 +51,7 @@ function cardCell(){
 
     for(var i=0; i<cells.length;i++){
         newCard=randomCard();
-        var closedCard=path +"10.png";
+
         var image= new Image();
         image.src = path + newCard;
         if($.inArray( newCard, usedCards )<0){
@@ -74,7 +62,6 @@ function cardCell(){
                 "background-repeat": "no-repeat"
              });
             usedCards.push(newCard);
-
         }
         else{
             var index=$.inArray( newCard, imageArray );
@@ -91,33 +78,26 @@ function cardCell(){
     }
 }
 
-// $("img").addClass("active");
-
-
 function openCard(e){
     var $target = $(e.target);
     
     if($("img.active").length==2){
-       
+ 
         $("img").removeClass("active");
-        // $("img").addClass("unactive");
-  
 
     }
     if($target.hasClass("col")){
+  
         $target.find("img").removeClass("active");
-
-
-
+  
     }
     else{
+
         $target.addClass("active"); 
-
-
 
     }
     if($("img.active").length==2){
-       
+        
         compareCards( $("img.active"));
         
     }
@@ -125,44 +105,44 @@ function openCard(e){
 };
 
 function compareCards(clickedCards){
-  
     var backgroundClickedFirstCard=$(clickedCards).eq(0).parent().attr("style");
     var backgroundClickedSecondCard=$(clickedCards).eq(1).parent().attr("style");
-   
+    
     if(backgroundClickedFirstCard==backgroundClickedSecondCard){
     
         $(clickedCards).remove();
         
         addPoints=Number(score.text())+100;
         score.text(addPoints); 
-        if( highScore.text()< addPoints ){
-            highScore.text(addPoints);
-            // localStorage.highScore= addPoints;
-        }
+      
         if($("img").length==0){
-            resulWrapper.addClass("active");
-            $("#win").removeClass("active");
-            $(".table").addClass("darken")
-            $("#finalScore").append(score.text());
-            $("#newBest").addClass("active");
-           
-            $(".timer div").stop();
-            addPoints=Number(score.text())+Math.round( $(".timer div").width());
-            highScore.text(addPoints);
-            allTimeBest.append(highScore.text());
-    
-    
-            console.log($(".timer div").width());
-    
-            if(Number(localStorage.highScore)  < highScore.text()){
-                localStorage.highScore=highScore.text();
-                $("#newBest").removeClass("active");
-            }
-        }
-       
+            victory();
+            
+        }     
     }
+       
    else if(Number(score.text())>0){
         addPoints=Number(score.text())-10;
         score.text(addPoints); 
     }
+}
+
+function victory(){
+    resulWrapper.addClass("hidden");
+    $("#win").removeClass("hidden");
+    $(".table").addClass("darken")
+    $("#newBest").addClass("hidden");
+
+    $(".timer div").stop();
+
+    addPoints=Number(score.text())+Math.round( $(".timer div").width());
+
+    $("#finalScore").append(score.text(addPoints));
+
+    if(Number(localStorage.highScore)  < score.text()){
+        localStorage.highScore=score.text();
+        
+        $("#newBest").removeClass("hidden");
+    }
+    allTimeBest.append(localStorage.highScore);
 }
